80e094d6b28dc6de49308552df430f27212fe7e3
Vignesh <automationbytes@gmail.com>
2023-10-04 20:24:05 +0530

    Added Get Examples

---
+++ src/test/java/RestAssuredExamples/GetReq.java
@@ -0,0 +1,34 @@
+package RestAssuredExamples;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import junit.framework.Assert;
+import org.testng.annotations.Test;
+
+public class GetReq {
+
+    //public static void main(String[] args) {
+
+    @Test
+    public void getReqEx()
+    {
+        RestAssured.baseURI ="http://tosca-webservice-ng.azurewebsites.net";
+
+        Response response = RestAssured
+                .when()
+                .get("/api/Coffees_V4")
+                .then()
+                .extract()
+                .response();
+
+        System.out.println(response.asPrettyString());
+        System.out.println("Status Code :"+response.statusCode());
+//
+//        if(response.statusCode() == 200){
+//
+//        }
+
+        Assert.assertEquals(204
+                , response.statusCode());
+    }
+}
